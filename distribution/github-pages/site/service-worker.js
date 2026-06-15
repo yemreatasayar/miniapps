8cdfebe6b00add835b6f6c8988ea8ad07b980391
Yusuf Emre Atasayar <yusufemreatasayar@Mac.fritz.box>
2026-06-15 18:32:06 +0200

    Disable public logo tooltips on mobile

--- distribution/github-pages/site/service-worker.js
+++ distribution/github-pages/site/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "20260615t162421620z";
+const CACHE_VERSION = "20260615t163051234z";
 const CACHE_PREFIX = "miniapps-github-pages";
 const SHELL_CACHE = `${CACHE_PREFIX}-shell-${CACHE_VERSION}`;
 const APP_CACHE = `${CACHE_PREFIX}-apps-${CACHE_VERSION}`;
@@ -21,8 +21,8 @@ const SHELL_PRECACHE_URLS = [
   "./assets/image-format-converter-card.svg",
   "./assets/image-toolkit-card.svg",
   "./assets/image-upscaler-card.svg",
-  "./assets/index-CSNQAGg7.js",
-  "./assets/index-u13HEgbR.css",
+  "./assets/index-Bt0GOsTr.css",
+  "./assets/index-ItQX-e2y.js",
   "./assets/miniapps-icon-maskable.svg",
   "./assets/miniapps-icon.svg",
   "./assets/miniapps-logo.svg",
